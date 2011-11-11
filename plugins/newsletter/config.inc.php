f68e8497bf7f589a430a62bdda1089e190ce01f5
Jan Kristinus <jan.kristinus@yakamara.de>
2011-11-11 18:11:29 +0100

    newsletter fixed

--- plugins/newsletter/config.inc.php
+++ plugins/newsletter/config.inc.php
@@ -6,15 +6,9 @@
  * @version 1.0
  */ 
 
-if ($REX["REDAXO"] 
-		&& 
-		$REX['USER'] 
-		&& 
-		($REX['USER']->isAdmin("rights","admin[]") || $REX['USER']->isValueOf("rights","community[admin]") || $REX['USER']->isValueOf("rights","community[setup]"))
-)
+if ($REX["REDAXO"] && $REX['USER'] && $REX['USER']->isAdmin("rights","admin[]"))
 {
-	// Diese Seite noch extra einbinden
-	$REX['ADDON']['community']['SUBPAGES'][] = array('plugin.newsletter','Newsletter');
+  $REX['ADDON']['community']['SUBPAGES'][] = array('plugin.newsletter','Newsletter');
 }
 
 $REX['ADDON']['NEWSLETTER_TEXT'] = FALSE;
@@ -28,56 +22,56 @@ $REX["ADDON"]["community"]["ff"][] = "last_newsletterid";
 function rex_newsletter_sendmail($userinfo, $mail_from_email, $mail_from_name, $mail_subject, $mail_body_text, $mail_body_html)
 {
 
-	global $REX;
+  global $REX;
 
 
-	if(trim($userinfo["email"]) == "")
-		return FALSE;
+  if(trim($userinfo["email"]) == "")
+    return FALSE;
 
-	$mail = new rex_mailer();
-	$mail->AddAddress($userinfo["email"]);
-	$mail->From = $mail_from_email;
-	$mail->FromName = $mail_from_name;
+  $mail = new rex_mailer();
+  $mail->AddAddress($userinfo["email"]);
+  $mail->From = $mail_from_email;
+  $mail->FromName = $mail_from_name;
 
-	$mail->Subject = $mail_subject;
+  $mail->Subject = $mail_subject;
 
-	if(trim($mail_body_html) != "")
-	{
-		$mail->Body = $mail_body_html;
-		$mail->AltBody = $mail_body_text;
-		foreach($userinfo as $k => $v)
-		{
-			$mail->Body = str_replace( "###".$k."###",$v,$mail->Body);
-			$mail->Body = str_replace( "###".strtoupper($k)."###",$v,$mail->Body);
-			$mail->Body = str_replace( "+++".$k."+++",urlencode($v),$mail->Body);
-			$mail->Body = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Body);
-			$mail->Subject = str_replace( "###".$k."###",$v,$mail->Subject);
-			$mail->Subject = str_replace( "###".strtoupper($k)."###",$v,$mail->Subject);
-			$mail->Subject = str_replace( "+++".$k."+++",urlencode($v),$mail->Subject);
-			$mail->Subject = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Subject);
-			$mail->AltBody = str_replace( "###".$k."###",$v,$mail->AltBody);
-			$mail->AltBody = str_replace( "###".strtoupper($k)."###",$v,$mail->AltBody);
-			$mail->AltBody = str_replace( "+++".$k."+++",urlencode($v),$mail->AltBody);
-			$mail->AltBody = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->AltBody);
-		}
-	}else
-	{
-		$mail->Body = $mail_body_text;
-		foreach($userinfo as $k => $v)
-		{
-			$mail->Body = str_replace( "###".$k."###",$v,$mail->Body);
-			$mail->Body = str_replace( "###".strtoupper($k)."###",$v,$mail->Body);
-			$mail->Body = str_replace( "+++".$k."+++",urlencode($v),$mail->Body);
-			$mail->Body = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Body);
-			$mail->Subject = str_replace( "###".$k."###",$v,$mail->Subject);
-			$mail->Subject = str_replace( "###".strtoupper($k)."###",$v,$mail->Subject);
-			$mail->Subject = str_replace( "+++".$k."+++",urlencode($v),$mail->Subject);
-			$mail->Subject = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Subject);
-		}
-	
-	
-	}	
+  if(trim($mail_body_html) != "")
+  {
+    $mail->Body = $mail_body_html;
+    $mail->AltBody = $mail_body_text;
+    foreach($userinfo as $k => $v)
+    {
+      $mail->Body = str_replace( "###".$k."###",$v,$mail->Body);
+      $mail->Body = str_replace( "###".strtoupper($k)."###",$v,$mail->Body);
+      $mail->Body = str_replace( "+++".$k."+++",urlencode($v),$mail->Body);
+      $mail->Body = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Body);
+      $mail->Subject = str_replace( "###".$k."###",$v,$mail->Subject);
+      $mail->Subject = str_replace( "###".strtoupper($k)."###",$v,$mail->Subject);
+      $mail->Subject = str_replace( "+++".$k."+++",urlencode($v),$mail->Subject);
+      $mail->Subject = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Subject);
+      $mail->AltBody = str_replace( "###".$k."###",$v,$mail->AltBody);
+      $mail->AltBody = str_replace( "###".strtoupper($k)."###",$v,$mail->AltBody);
+      $mail->AltBody = str_replace( "+++".$k."+++",urlencode($v),$mail->AltBody);
+      $mail->AltBody = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->AltBody);
+    }
+  }else
+  {
+    $mail->Body = $mail_body_text;
+    foreach($userinfo as $k => $v)
+    {
+      $mail->Body = str_replace( "###".$k."###",$v,$mail->Body);
+      $mail->Body = str_replace( "###".strtoupper($k)."###",$v,$mail->Body);
+      $mail->Body = str_replace( "+++".$k."+++",urlencode($v),$mail->Body);
+      $mail->Body = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Body);
+      $mail->Subject = str_replace( "###".$k."###",$v,$mail->Subject);
+      $mail->Subject = str_replace( "###".strtoupper($k)."###",$v,$mail->Subject);
+      $mail->Subject = str_replace( "+++".$k."+++",urlencode($v),$mail->Subject);
+      $mail->Subject = str_replace( "+++".strtoupper($k)."+++",urlencode($v),$mail->Subject);
+    }
+  
+  
+  } 
 
-	return $mail->Send();
+  return $mail->Send();
 
 }
